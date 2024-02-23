c654aa5e4b72a9de3ecca3dd9c056c6eea3e2c37
argentum88 <argentimgipan@gmail.com>
2024-02-23 12:13:51 +0500

    Try again fix preview with modal

--- assets/app.js
+++ assets/app.js
@@ -15,7 +15,11 @@ import {Modal} from "bootstrap";
 
 document.addEventListener('turbo:before-cache', function () {
     if (document.body.classList.contains('modal-open')) {
-        const modal = Modal.getInstance(document.querySelector('.modal'));
+        const modalEl = document.querySelector('.modal');
+        const modal = Modal.getInstance(modalEl);
+        modalEl.classList.remove('fade');
+        modal._backdrop._config.isAnimated = false;
         modal.hide();
+        modal.dispose();
     }
 });
